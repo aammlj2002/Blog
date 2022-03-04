20d1b25769436066e99221efbe759dd46de40034
Leo <aammlj2002@gmail.com>
2022-03-04 15:44:54 +0630

    add comment migration

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\Blog;
 use App\Models\Category;
+use App\Models\Comment;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
@@ -16,12 +17,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::truncate();
-        Blog::truncate();
-        Category::truncate();
-
         $mgmg = User::factory()->create(["name"=>"Mg Mg", "username"=>"mgmg"]);
         $aungaung = User::factory()->create(["name"=>"Aung Aung", "username"=>"aungaung"]);
+        User::factory()->create(["name"=>"zoro", "username"=>"zoro123", "email"=>"zoro@gmail.com"]);
 
         $frontend = Category::factory()->create(["name"=>"frontend", "slug"=>"frontend"]);
         $backend = Category::factory()->create(["name"=>"backend", "slug"=>"backend"]);
@@ -30,5 +28,8 @@ class DatabaseSeeder extends Seeder
         Blog::factory(10)->create(["user_id"=>$mgmg->id, "category_id"=>$backend->id]);
         Blog::factory(10)->create(["user_id"=>$aungaung->id, "category_id"=>$frontend->id]);
         Blog::factory(10)->create(["user_id"=>$aungaung->id, "category_id"=>$backend->id]);
+
+        Comment::factory()->create(["body"=>"hello", "user_id"=>1, "blog_id"=>2]);
+        Comment::factory()->create(["body"=>"hello", "user_id"=>2, "blog_id"=>3]);
     }
 }
