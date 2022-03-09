d5974033014dad7a2af01cc11da9d43d0f3f1e14
Leo <aammlj2002@gmail.com>
2022-03-09 15:54:24 +0630

    show comment reply in blade

--- resources/views/components/comment-section.blade.php
+++ resources/views/components/comment-section.blade.php
@@ -11,7 +11,7 @@
                 @auth
                 <form method="POST" class="mb-4" action="/blogs/{{$blog}}/comment">
                     @csrf
-                    <textarea class="form-control" rows="3" name="body"
+                    <textarea class="form-control" rows="2" name="body"
                         placeholder="Join the discussion and leave a comment!"></textarea>
                     @error("body")
                     <p class="text-danger">{{$message}}</p>
@@ -21,31 +21,46 @@
                     </div>
                 </form>
                 @endauth
-                <!-- Single comment-->
-                @foreach ($comments as $comment)
-                <div class="d-flex mb-3">
-                    <div class="flex-shrink-0"><img class="rounded-circle" src="{{$comment->author->avatar}}"
-                            alt="..." /></div>
-                    <div class="ms-3">
-                        <div class="fw-bold">{{$comment->author->name}}</div>
-                        <p class="text-muted">{{$comment->created_at->diffForHumans()}}</p>
-                        {{$comment->body}}
-                    </div>
-                </div>
-                @auth
-                <form method="POST" class="mb-4" action="/blogs/{{$blog}}/comment">
-                    @csrf
-                    <textarea class="form-control" rows="3" name="body"
-                        placeholder="Join the discussion and leave a comment!"></textarea>
-                    @error("body")
-                    <p class="text-danger">{{$message}}</p>
-                    @enderror
-                    <div class="d-flex justify-content-end">
-                        <button type="submit" class="btn btn-primary mt-3">reply</button>
-                    </div>
-                </form>
-                @endauth
-                @endforeach
             </div>
+            <!-- Single comment-->
+            @foreach ($comments as $comment)
+            <div class="d-flex mb-3 ml-3">
+                <div class="flex-shrink-0 m-3"><img class="rounded-circle" src="{{$comment->author->avatar}}"
+                        alt="..." />
+                </div>
+                <div class="ms-3">
+                    <div class="fw-bold">{{$comment->author->name}}</div>
+                    <p class="text-muted m-0">{{$comment->created_at->diffForHumans()}}</p>
+                    {{$comment->body}}
+                    @foreach ($comment->replies as $reply)
+                    <div class="d-flex mt-4">
+                        <div class="flex-shrink-0"><img class="rounded-circle" src="{{$reply->author->avatar}}"
+                                alt="..." /></div>
+                        <div class="ms-3">
+                            <div class="fw-bold">{{$reply->author->name}}</div>
+                            <p class="text-muted m-0">{{$reply->created_at->diffForHumans()}}</p>
+                            {{$reply->body}}
+                        </div>
+                    </div>
+                    @endforeach
+                </div>
+            </div>
+            {{-- comment reply form --}}
+            @auth
+            <form method="POST" class="mb-4 p-3" action="/blogs/{{$blog}}/comment">
+                @csrf
+                <textarea class="form-control" rows="2" name="body"
+                    placeholder="Join the discussion and leave a comment!"></textarea>
+                @error("body")
+                <p class="text-danger">{{$message}}</p>
+                @enderror
+                <div class="d-flex justify-content-end">
+                    <button type="submit" class="btn btn-primary mt-3">Reply</button>
+                </div>
+            </form>
+            @endauth
+            <hr>
+            @endforeach
         </div>
+</div>
 </div>
