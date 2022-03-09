1bb9f0a2b460c160933c4c78fd5c9afaf88061ef
Leo <aammlj2002@gmail.com>
2022-03-09 14:54:30 +0630

    add reply comment form

--- resources/views/components/comment-section.blade.php
+++ resources/views/components/comment-section.blade.php
@@ -23,7 +23,28 @@
                 @endauth
                 <!-- Single comment-->
                 @foreach ($comments as $comment)
-                <x-comment :comment="$comment" />
+                <div class="d-flex mb-3">
+                    <div class="flex-shrink-0"><img class="rounded-circle" src="{{$comment->author->avatar}}"
+                            alt="..." /></div>
+                    <div class="ms-3">
+                        <div class="fw-bold">{{$comment->author->name}}</div>
+                        <p class="text-muted">{{$comment->created_at->diffForHumans()}}</p>
+                        {{$comment->body}}
+                    </div>
+                </div>
+                @auth
+                <form method="POST" class="mb-4" action="/blogs/{{$blog}}/comment">
+                    @csrf
+                    <textarea class="form-control" rows="3" name="body"
+                        placeholder="Join the discussion and leave a comment!"></textarea>
+                    @error("body")
+                    <p class="text-danger">{{$message}}</p>
+                    @enderror
+                    <div class="d-flex justify-content-end">
+                        <button type="submit" class="btn btn-primary mt-3">reply</button>
+                    </div>
+                </form>
+                @endauth
                 @endforeach
             </div>
         </div>
